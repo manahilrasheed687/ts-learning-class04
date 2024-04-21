5f6a1452d5fc8e6f62c67312e80e56883d127eb7
Manahil rasheed <manahilrasheed507@gmail.com>
2024-04-21 04:59:32 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+// Addition
+var num1 = 1;
+var num2 = 2;
+var show = (num1 + num2);
+console.log(show);
+// Subtraction
+var num3 = 12;
+var num4 = 15;
+var show2 = (num3 - num4);
+console.log(show2);
+// Multiplication
+var num5 = 15;
+var num6 = 10;
+var show3 = (num5 * num5);
+console.log(show3);
+// Division 
+var num7 = 15;
+var num8 = 20;
+var show4 = (num7 / num8);
+console.log(show4);
---
+++ main.ts
@@ -0,0 +1,25 @@
+// Addition
+
+let num1 = 1;
+let num2 = 2;
+let show = (num1 + num2);
+console.log(show)
+
+
+// Subtraction
+let num3 = 12;
+let num4 = 15;
+let show2 = (num3 - num4);
+console.log(show2)
+
+// Multiplication
+let num5 = 15;
+let num6 = 10;
+let show3 = (num5 * num5);
+console.log(show3)
+
+// Division 
+let num7 = 15;
+let num8 = 20;
+let show4 = (num7 / num8)
+console.log(show4);
